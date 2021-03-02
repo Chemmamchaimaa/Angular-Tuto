1092bf014b31b3d94a31ad0d95db522109fb3813
Chaimaa Chemmam <cchemmam@SMB.SQLI.COM>
2021-03-02 17:37:41 +0100

    First commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Hero } from "./heroes/hero";
+import { HEROES } from "./heroes/mock-heroes";
+import {Observable ,of} from 'rxjs';
+import { MessageService } from './message.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService : MessageService) { }
+
+  getHeroes() : Observable<Hero[]>{
+    const heroes = of(HEROES);
+    this.messageService.add('HeroSerice : fetch heroes');
+    return heroes;
+  }
+
+
+}
